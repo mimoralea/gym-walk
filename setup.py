b34f936290d1c0b48c9a1d61ae8ae9464eb16e64
Miguel Morales <mimoralea@gmail.com>
2018-02-08 16:01:31 -0700

    Add file structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    version='0.0.1',
+    description='Gym walk environment - useful to replicate Random Walk experiments',
+    url='https://github.com/rldm/gym-walk',
+    author='Miguel Morales',
+    author_email='mimoralea@gmail.com',
+    packages=['gym_walk'],
+    license='MIT License',
+    install_requires=['gym'],
+)
